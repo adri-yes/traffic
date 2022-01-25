c5462d759b624bbc9ca4a7716edcf36ba06cbd72
aleonardo <adrienne@apollographql.com>
2022-01-25 14:43:59 -0800

    modified

--- index.js
+++ index.js
@@ -33,38 +33,46 @@ let clients = [
 let randomClient = clients[Math.floor(Math.random() * clients.length)];
 
 let versions = [
-    '1.0',
-    '1.1',
-    '1.2',
-    '1.4',
-    '1.5',
-    '1.7',
-
+  '1.0',
+  '1.1',
+  '1.2',
+  '1.4',
+  '1.5',
+  '1.7',
+  '1.8',
+  '1.9',
+  '2.0',
+  '2.2',
+  '2.5',
+  '3.0'
 ]
 let randomVersion = versions[Math.floor(Math.random() * versions.length)];
 
-let oddsArray = [1, 1, 1, 1]
+let oddsArray = [1, 1, 1, 1, 2, 2, 2, 3, 3, 4 ]
 let random = oddsArray[Math.floor(Math.random() * oddsArray.length)];
 
+let randomProduct = Math.floor(Math.random() * 15 + 1);
+
+let randomUser = Math.floor(Math.random() * 8 + 1);
 
 // let possibleTimes = [...Array(300).keys()].map(x => x + 30 )
 let possibleTimes = [...Array(10).keys()].map(x => x + 2 )
 let times = possibleTimes[Math.floor(Math.random() * possibleTimes.length)];
 
 if(random === 1) {
-    for (let i = 0; i < times; i++) {
-        fetchAllFlights(randomClient, randomVersion);
-    }
+  for (let i = 0; i < times; i++) {
+      fetchAllProducts(randomClient, randomVersion);
+  }
 } else if (random === 2) {
-    for (let i = 0; i < times; i++) {
-        fetchOneProduct(randomClient, randomVersion, randomProduct);
-    }
+  for (let i = 0; i < times; i++) {
+      fetchOneProduct(randomClient, randomVersion, randomProduct);
+  }
 } else if (random === 3) {
-    for (let i = 0; i < times; i++) {
-        fetchUserAccount(randomClient, randomVersion, randomUser);
-    } 
+  for (let i = 0; i < times; i++) {
+      fetchUserAccount(randomClient, randomVersion, randomUser);
+  } 
 } else {
-    for (let i = 0; i < 4; i++) {
-        legacyClientAccount(randomUser);
-    }
+  for (let i = 0; i < 4; i++) {
+      legacyClientAccount(randomUser);
+  }
 };
