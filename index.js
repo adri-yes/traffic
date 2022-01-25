e0eb9939c5edf06fca1e5b76577de1c4c6af0107
aleonardo <adrienne@apollographql.com>
2022-01-25 14:37:14 -0800

    modified index.js

--- index.js
+++ index.js
@@ -1,18 +1,70 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+const fetch = require('node-fetch');
 
-const cache = new InMemoryCache();
+async function fetchAllProducts(randomClient, randomVersion) {
+    const response = await fetch('https://apollo-gateway-t4modcxifa-uc.a.run.app', {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        'apollographql-client-name': randomClient,
+        'apollographql-client-version': randomVersion
+        },
+        body:JSON.stringify({ query: `query getAllFlights {
+        flights {
+            flightNumber
+            departureDate
+            }
+        }
+        }`})
+    })
+    .then((res) => res.json())
+    .then((result) => {
+        console.log(result.data)
+        return result.data
+    });
+    return response
+};
 
-const client = new ApolloClient({
-  // Provide required constructor fields
-  cache: cache,
-  uri: 'https://apollo-gateway-t4modcxifa-uc.a.run.app',
-  headers: {
-      'client-name': 'web-client',
-      'client-version': '1.1',
-      'queryDeduplication': false
-  },
-});
 
-queries = [
-  gql("query getFlights { flights { flightNumber, departureDate }}")
+let clients = [
+    'iOS',
+    'Web',
+    'Android'
 ]
+let randomClient = clients[Math.floor(Math.random() * clients.length)];
+
+let versions = [
+    '1.0',
+    '1.1',
+    '1.2',
+    '1.4',
+    '1.5',
+    '1.7',
+
+]
+let randomVersion = versions[Math.floor(Math.random() * versions.length)];
+
+let oddsArray = [1, 1, 1, 1]
+let random = oddsArray[Math.floor(Math.random() * oddsArray.length)];
+
+
+// let possibleTimes = [...Array(300).keys()].map(x => x + 30 )
+let possibleTimes = [...Array(10).keys()].map(x => x + 2 )
+let times = possibleTimes[Math.floor(Math.random() * possibleTimes.length)];
+
+if(random === 1) {
+    for (let i = 0; i < times; i++) {
+        fetchAllFlights(randomClient, randomVersion);
+    }
+} else if (random === 2) {
+    for (let i = 0; i < times; i++) {
+        fetchOneProduct(randomClient, randomVersion, randomProduct);
+    }
+} else if (random === 3) {
+    for (let i = 0; i < times; i++) {
+        fetchUserAccount(randomClient, randomVersion, randomUser);
+    } 
+} else {
+    for (let i = 0; i < 4; i++) {
+        legacyClientAccount(randomUser);
+    }
+};
